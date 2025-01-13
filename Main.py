da8d42530fdcc2d9445485553c6213a6cc9ea19c
Mashen_Taterz <ty_beach@live.com>
2025-01-12 20:17:02 -0800

    First

---
+++ Main.py
@@ -0,0 +1,69 @@
+import Translations
+import random
+
+# Start off asking the players name.
+name = input ("Hello! What is your name? ")
+print (f"Hi, {name} ")
+
+# Ask if we are ready to start the game.
+while True:
+    ready = input ("Are you ready to play a game? ").lower() # Convert input to lowercase.
+    if ready == "yes":
+        print ("Great!")
+        break # Exit loop.
+    elif ready == "no":
+        print ("Maybe another time!")
+        break # Exit loop.
+    else: 
+        print ("I did not quite get that. Lets try again!") # Repeat the loop.
+
+# Chose a game you want to play (ask or answer).
+while True:
+    game_type = input ("Do you want me to translate English words into Japanese (A), or answer my promtps (B)? ").lower()
+    if game_type == "a":
+        print ("Ok, you give me a word and I will attemp to translate for you.")
+        break 
+    elif game_type == "b":
+        print ("Ok, You will have to translate my Japanese into English.")
+        break
+    else:
+        print ("Try game type A or B. ")
+
+# Game type A : Ask to translate English to Japanese.
+if game_type == "a":
+    while True:
+        word = input ("Enter a word you would like translated: ").lower() # The user provides an English word.
+        if word in Translations.translations: # Check if the word is in the dictionary (key).
+            translation = Translations.translations[word] # Look up the translation (value).
+            print (f"The Japanese translation for '{word}' is: {translation}")
+        else:
+            print (f"Sorry I don't have the translation for '{word}' yet.")
+        play_again = input ("Do you want to try another word? (yes/no) ").lower()
+        if play_again != "yes": # Anything but a "yes" will end the game.
+            print("Thanks for playing!")
+            break
+
+# Game type B : Answer the Japanese promt to English. 
+elif game_type == "b":
+    #import random.
+    # list of words to translate from Japanese to English.
+    japanese_words = list(Translations.translations.keys())
+
+    while True: 
+        # Pick a random word for player to translate to English.
+        word = random.choice(japanese_words) # Randomly pick a Japanese word (key).
+        translation = Translations.translations[word] # Look up the Japanese translation (value).
+
+        # Get the players translation.
+        player_translation = input(f"What is the English word for '{translation}'? ")
+
+        # Check if players translation is correct.
+        if player_translation == word:
+            print("Correct! Well done.")
+        else:
+            print(f"Oops! The correct translation is '{word}'.")
+
+        play_again = input("Do you want to try another word? (yes/no) ").lower()
+        if play_again != "yes":
+            print("Thanks for playing!")
+            break
---
+++ Translations.py
@@ -0,0 +1,37 @@
+# Translations for English and Japanese with Romaji for pronounciation
+
+translations = {
+# Common words
+    "hello": "こんにちは (Konnichiwa)",
+    "goodbye": "さようなら (Sayōnara)",
+    "this": "これ (Kore)",
+    "that": "それ (Sore)",
+    "here": "ここ (Koko)",
+    "there": "そこ (Soko)",
+    "over there": "あそこ (Asoko)",
+    "he": "彼 (Kare)",
+    "she": "彼女 (Kanojo)",
+    "they": "彼ら (Karera)",
+    "hot": "暑い (Atsui)",
+    "cold": "寒い (Samui)",
+# Colors
+    "red": "赤 (Aka)",
+    "blue": "青 (Ao)",
+    "green": "緑 (Midori)",
+    "yellow": "黄色 (Kiiro)",
+    "purple": "紫 (Murasaki)",
+    "orange": "オレンジ (Orenji)",
+    "white": "白 (Shiro)",
+    "black": "黒 (Kuro)",
+# Numbers
+    "1": "一 (Ichi)",
+    "2": "二 (Ni)",
+    "3": "三 (San)",
+    "4": "四 (Shi/Yon)",
+    "5": "五 (Go)",
+    "6": "六 (Roku)",
+    "7": "七 (Shichi/Nana)",
+    "8": "八 (Hachi)",
+    "9": "九 (Kyū)",
+    "10": "十 (Jū)"
+}
